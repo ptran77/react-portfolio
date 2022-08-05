cca688219e4e3e48e466ed3875eeae04db0ec67c
Peter Tran <pettran866@gmail.com>
2022-08-05 14:14:02 -0700

    made Header component to have title + Nav Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import Nav component to use inside header
+import Nav from '../Nav';
+
+function Header(props) {
+  // destructure props
+  const {
+    navOptions = [],
+    currentNavOp,
+    setCurrentNavOp,
+  } = props;
+
+  // use for nav component to decide which should be highlighted when selected
+  const [navOpSelected, setNavOpSelected] = useState(false);
+
+  return (
+    <header>
+      <a href="/">Peter Tran</a>
+      <Nav
+        navOptions={navOptions}
+        currentNavOp={currentNavOp}
+        setCurrentNavOp={setCurrentNavOp}
+        navOpSelected={navOpSelected}
+        setNavOpSelecte={setNavOpSelected}
+      />
+    </header>
+  )
+}
+
+export default Header;
