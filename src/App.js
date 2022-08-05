3a44506b5bd4d8479c66d791a4becc8a0f72f487
Peter Tran <pettran866@gmail.com>
2022-08-05 14:04:39 -0700

    added header section to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import Header from './components/Header';
+//import Footer from './components/Footer';
+
 function App() {
+  // navigation options
+  const navOptions = useState(['About Me', 'Portfolio', 'Contact', 'Resume']);
+
+  // Setting the current render to About Me
+  // setCurrentNavOp is used to change the navOption
+  const [currentNavOp, setCurrentNavOp] = useState(navOptions[0]);
+
+  // use for nav component to decide which should be highlighted when selected
+  const [navOpSelected, setNavOpSelected] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header
+      navOptions={navOptions}
+      currentNavOp={currentNavOp}
+      setCurrentNavOp={setCurrentNavOp}
+      navOpSelected={navOpSelected}
+      setNavOpSelecte={setNavOpSelected}
+    />
   );
 }
 
