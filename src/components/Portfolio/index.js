3eaddf2ec9d2910379c43c97ea0539dae671ff6a
Peter Tran <pettran866@gmail.com>
2022-08-05 20:45:26 -0700

    added project list to portfolio array

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Project from '../Project';
+
+function Portfolio() {
+  const projects = useState([
+    {
+      name: "Garden Planner",
+      link: "https://garden-planner-90482.herokuapp.com/",
+      github: "https://github.com/ptran77/project-2",
+      tool: "Full-Stack"
+    },
+    {
+      name: "Anime4You",
+      link: "https://diegorivera1110.github.io/anime4you/",
+      github: "https://github.com/Diegorivera1110/anime4you",
+      tool: "Front-End"
+    },
+    {
+      name: "Budget Tracker",
+      link: "https://budget-tracker-26833.herokuapp.com/",
+      github: "https://github.com/ptran77/budget-tracker",
+      tool: "PWA"
+    },
+    {
+      name: "Social Network API",
+      link: "https://drive.google.com/file/d/1XpSZu62JcIR9g-mRF5qgI4WSOmDGrXy-/view",
+      github: "https://github.com/ptran77/social-network-api",
+      tool: "NoSQL"
+    },
+    {
+      name: "Tech Blog",
+      link: "https://pt-tech-blog-2022.herokuapp.com/",
+      github: "https://github.com/ptran77/tech-blog",
+      tool: "MVC"
+    },
+    {
+      name: "E-commerce Back End",
+      link: "https://drive.google.com/file/d/1tRGX0NGGXI8P3nzUzeRMnxcY10K1DDLM/view?usp=sharing",
+      github: "https://github.com/ptran77/e-commerce-back-end",
+      tool: "ORM"
+    }
+  ]);
+
+  return (
+    <div>
+      <h2>Portfolio</h2>
+    </div>
+  )
+}
+
+export default Portfolio;
