027fa774d2b192aa0fbd82ad8d202c628d034f9d
Peter Tran <pettran866@gmail.com>
2022-08-06 15:31:31 -0700

    stylized footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer className="bg-dark text-center p-3">
       <a href='https://github.com/ptran77'><i className="fa fa-github" /></a>
       <a href='https://www.linkedin.com/in/peter-tran-b9a02620b/'><i className="fa fa-linkedin-square" /></a>
       <a href='https://twitter.com/PeterTr98757311'><i className="fa fa-twitter" /></a>
